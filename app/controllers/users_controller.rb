5042a1392ac24ffc567feb70a7efeaaaf21a3765
Estelle Recuero <estelle-ad@outlook.com>
2020-02-18 16:28:09 +0100

    functional version with all user views

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,19 @@
 class UsersController < ApplicationController
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+      if @user.update(first_name: params[:first_name], last_name: params[:last_name], description: params[:description])
+        flash[:success] = "Your account informations have been modified"
+        redirect_to @user
+      else
+        render :edit
+      end
+    end
 end
